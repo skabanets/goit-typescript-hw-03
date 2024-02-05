49c8f47868443a988061a687bd0c270392504eb5
Sergey Kabanets <skabanets97@gmail.com>
2024-02-05 17:42:44 +0200

    fix task 4

--- src/4.ts
+++ src/4.ts
@@ -1,7 +1,7 @@
 class Key {
   private signature: number = Math.floor(Math.random() * 1000);
 
-  getSignature() {
+  getSignature(): number {
     return this.signature;
   }
 }
@@ -9,15 +9,16 @@ class Key {
 class Person {
   constructor(private key: Key) {}
 
-  getKey() {
+  getKey(): Key {
     return this.key;
   }
 }
 
 abstract class House {
   protected door: boolean = false;
-  protected key: Key;
   protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
 
   comeIn(person: Person): void {
     if (this.door) {
@@ -37,7 +38,7 @@ class MyHouse extends House {
 }
 
 const key = new Key();
-const house = new MyHouse();
+const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
