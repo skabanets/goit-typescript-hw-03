4517096e815f5a801cd25712297ff20e215c453b
Sergey Kabanets <skabanets97@gmail.com>
2024-02-05 16:58:38 +0200

    add tasks 1-4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,46 @@
-const key = new Key();
+class Key {
+  private signature: number = Math.floor(Math.random() * 1000);
 
-const house = new MyHouse(key);
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door === true;
+    }
+  }
+}
+
+const key = new Key();
+const house = new MyHouse();
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
